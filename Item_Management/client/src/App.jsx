e2219e4ae213799864a364bc584c166907246582
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2024-03-18 20:56:52 +0530

    Added routes

--- Item_Management/client/src/App.jsx
+++ Item_Management/client/src/App.jsx
@@ -1,13 +1,14 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Item_main from './pages/Item_main';
 import Item_add from './pages/Item_add';
+import Item_Update from './pages/Item_Update';
 
 export default function App() {
   return<BrowserRouter>
   <Routes>
     <Route path='/' element={<Item_main/>} />
     <Route path='/Item_add' element={<Item_add/>} />
-    <Route path='/Item_Update' element={<Item_main/>} />
+    <Route path='/Item_Update' element={<Item_Update/>} />
   </Routes>
   </BrowserRouter>;
   
