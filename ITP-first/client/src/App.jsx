b9b7bbbe199ef827e9ff4a563b76c047cfddd92d
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2024-03-18 20:23:20 +0530

    Rename ITP-first
    This commit renamed the ITP-first file to Item_Management

--- ITP-first/client/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
-  
-}
---
+++ Item_Management/client/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function App() {
+  return <h1 className='text-red-500'>App</h1>;
+  
+}
