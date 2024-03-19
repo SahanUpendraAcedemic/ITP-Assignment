98dd7391e0f61a305bcbab332e687f97422090db
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2024-03-19 09:28:26 +0530

    reset app.jsx file

--- Supplier01/src/App.jsx
+++ Supplier01/src/App.jsx
@@ -1,19 +1,6 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom"
-import home from "./pages/home"
-import profile from "./pages/profile"
-import singin from "./pages/singin"
-import singup from "./pages/singup"
+import React from "react";
 
+export default function App () {
 
-export default function App() {
-  return <BrowserRouter>
-
-      <Routes>
-         <Route path="/" element= {<home/> } />
-         <Route path="/profile" element= {<profile/> } />
-         <Route path="/singin" element= {<singin/> } />
-         <Route path="/singup" element= {<singup/> } />
-      </Routes>
-  </BrowserRouter>
-  
+    return <h1 className="text-red-500">App</h1>
 }
