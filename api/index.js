ff4e0e1279f05db9530aae9f865339d717c5c8c8
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2024-03-19 15:34:35 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import experess from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = experess();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('connect to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(3000, () => {
     console.log("Sever listening on port 3000 ")
