4269aec9f530490feda693c85c62dac33e0521d3
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2024-03-19 22:46:49 +0530

    connected to the DB

--- Item_Management/api/index.js
+++ Item_Management/api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//Database connection to mongoDB cloud with console feedback and error feedback
+mongoose.connect(process.env.MONGO) //added a .env file const for better protection on DB connection
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const app=express();
 
