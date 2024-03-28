b97fcb301d0d4df505f527521a70ae0e9cdfc07b
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2024-03-28 13:49:51 +0530

    deleted

--- api/controllers/supplier.controllers.js
+++
@@ -1,6 +0,0 @@
-export const test = (req, res) => {
-
-    res.json({
-        message: 'API is working !',
-    });
-};
--- api/routes/supplier.routes.js
+++
@@ -1,9 +0,0 @@
-import experess from 'express';
-import { test } from '../controllers/supplier.controllers.js';
-
-const router = experess.Router();
-
-router.get('/', test); 
-
-
-export default router;
