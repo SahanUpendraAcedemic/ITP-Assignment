5101d23b5bc445ce66a5e40080b4466af210af7d
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2024-03-28 19:25:36 +0530

    create pages and routes

--- supplier_management/src/App.jsx
+++ supplier_management/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Report from './pages/Report';
 
 export default function App() {
   return (
-    <h1 className='text-red-600'>App</h1>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element= {<Home/>}/>
+            <Route path='/login' element= {<Login/>}/>
+            <Route path='/register' element= {<Register/>}/>
+            <Route path='/profile' element= {<Profile/>}/>
+            <Route path='/report' element = {<Report/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
